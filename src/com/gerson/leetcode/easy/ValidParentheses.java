793077856a8dad11c2a5889a792b5d71085025c6
gezhizheng <gezhizheng@mvp01.com>
2019-06-18 13:53:26 +0800

    判断字符串列表括号的有效性

--- src/com/gerson/leetcode/easy/ValidParentheses.java
+++ src/com/gerson/leetcode/easy/ValidParentheses.java
@@ -1,9 +1,51 @@
 package com.gerson.leetcode.easy;
 
+import com.gerson.leetcode.structure.MyStack;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * @author gezz
  * @description 判断括号的有效性
  * @date 2019/6/18.
  */
 public class ValidParentheses {
+
+    public static Map<Character,Character> mapper = new HashMap<>();
+    static {
+        mapper.put(new Character('}'),new Character('{'));
+        mapper.put(new Character(')'),new Character('('));
+        mapper.put(new Character(']'),new Character('['));
+    }
+    public static boolean isValid(String str) {
+        if (str == null) {
+            return true;
+        }
+        MyStack<Character> stack = new MyStack<>();
+        char[] chars = str.toCharArray();
+        for (char c : chars) {
+            if (mapper.containsKey(c)) {
+                // popItem : { [ (
+                char popItem = stack.pop();
+                if (Objects.equals(mapper.get(c) , popItem)) {
+                    continue;
+                } else {
+                    return false;
+                }
+            } else if (mapper.containsValue(c)) {
+                stack.push(c);
+            } else {
+                continue;
+            }
+        }
+        return stack.getSize() == 0;
+    }
+
+    @Test
+    public void testValid() {
+        System.out.println(isValid("[[[[(shs{[e}]hew)]]]]"));
+    }
 }
