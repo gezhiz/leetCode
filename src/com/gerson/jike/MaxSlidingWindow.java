327afa352f7c2da309b54d26b64a78540a05fac4
gezhizheng <gezhizheng@mvp01.com>
2019-07-04 15:57:05 +0800

    获取窗口最大值，算法正确完成

--- src/com/gerson/jike/MaxSlidingWindow.java
+++ src/com/gerson/jike/MaxSlidingWindow.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * 获取滑动窗口最大值
@@ -40,16 +41,7 @@ public class MaxSlidingWindow {
      */
     @Test
     public void test1() {
-//        int[] nums = {9,2,9,4,10,54,1,99,78,2,4,5,2,33,45,2,4,5,6,5,8,234};
-        int[] nums = {5183,2271,3067,539,8939,2999,9264,737,3974,5846
-                ,-210,9278,5800,2675,6608,1133,-1,6018,9672,5179,9842
-                ,7424,-209,2988,2757,5984,1107,2644,-499,7234,7539,6525,
-                347,5718,-742,1797,5292,976,8752,8297,1312,3385,5924,2882,
-                6091,-282,2595,96,1906,8014,7667,5895,7283,7974,-167,7068,
-                3946,6223,189,1589,2058,9277,-302,8157,8256,5261,8067,1071,
-                9470,2682,8197,5632,753,3179,8187,9042,8167,4657,7080,7801,
-                5627,7917,8085,928,-892,-427,3685,4676,2431,8064,8537,343,
-                505,4352,2108,4399,66,2086,1922,9126,9460,393,443,5689,7595};
+        int[] nums = {9,2,9,4,10,54,1,99,78,2,4,5,2,33,45,2,4,5,6,5,8,234};
         int[] result = maxSlidingWindow(nums,5);
         if (result == null) {
             return;
@@ -57,6 +49,37 @@ public class MaxSlidingWindow {
         for (int i = 0; i < result.length; i++) {
             System.out.print(result[i] + " ");
         }
+    }
+
+    /**
+     * 最大值存放在队列最左边
+     * 每次从右边入队时，如果右边有更小的元素，则让其弹出.如果队列被清空，当前item就是最大值，记录当前最大值的索引，根据k和i的值来判定最大值是否已经划出了窗口
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (k == 0 || nums == null) {
+            return new int[0];
+        }
+        Deque<Integer> arrayDeque = new ArrayDeque<>(k);
+        int[] result = new int[nums.length - k + 1];
+        for (int i = 0; i < nums.length; i++) {
+            int item = nums[i];
+            //判断最大值是否在窗口内部
+            if (arrayDeque.size() > 0 && arrayDeque.getFirst() <= i - k) {
+                //无效
+                arrayDeque.pollFirst();
+            }
+            while (arrayDeque.size() > 0 && item >= nums[arrayDeque.getLast()]) {
+                arrayDeque.pollLast();
+            }
+            arrayDeque.addLast(i);
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[arrayDeque.getFirst()];
+            }
+        }
+        return result;
     }
 
     /**
@@ -99,7 +122,7 @@ public class MaxSlidingWindow {
      * @param k
      * @return
      */
-    public int[] maxSlidingWindow(int[] nums, int k) {
+    public int[] maxSlidingWindoWrong2(int[] nums, int k) {
         if (k == 0 || nums == null) {
             return new int[0];
         }
@@ -134,6 +157,37 @@ public class MaxSlidingWindow {
             arrayDeque.addLast(item);
             if (i >= k - 1) {
                 result[i - k + 1] = arrayDeque.getFirst();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 其他人的算法
+     */
+    public int[] maxSlidingWindow1(int[] nums, int k) {
+        if(nums == null || nums.length < 2){
+            return nums;
+        }
+        // 双向队列 保存当前窗口最大值的数组位置 保证队列中数组位置的数值按从大到小排序
+        LinkedList<Integer> queue = new LinkedList();
+        // 结果数组
+        int[] result = new int[nums.length-k+1];
+        // 遍历nums数组
+        for(int i = 0;i < nums.length;i++){
+            // 保证从大到小 如果前面数小则需要依次弹出，直至满足要求
+            while(!queue.isEmpty() && nums[queue.peekLast()] <= nums[i]){
+                queue.pollLast();
+            }
+            // 添加当前值对应的数组下标
+            queue.addLast(i);
+            // 判断当前队列中队首的值是否有效
+            if(queue.peek() <= i-k){
+                queue.poll();
+            }
+            // 当窗口长度为k时 保存当前窗口中最大值
+            if(i+1 >= k){
+                result[i+1-k] = nums[queue.peek()];
             }
         }
         return result;
