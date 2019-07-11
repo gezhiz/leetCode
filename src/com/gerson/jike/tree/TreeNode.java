8d574882a3b17a159ca172df761f6a30747e448b
gezhizheng <gezhizheng@mvp01.com>
2019-07-11 18:33:00 +0800

    二叉树 数据结构

---
+++ src/com/gerson/jike/tree/TreeNode.java
@@ -0,0 +1,19 @@
+package com.gerson.jike.tree;
+
+/**
+ * @author gezz
+ * @description 二叉树
+ * @date 2019/7/11.
+ */
+public class TreeNode {
+
+    private int val;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
