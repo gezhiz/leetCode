1ec34236e44358f3023b30f30310bb7d0d4842c6
gezhizheng <gezhizheng@mvp01.com>
2019-07-16 19:12:41 +0800

    平衡二叉树的基本实现

--- src/com/gerson/jike/tree/BinarySearchTree.java
+++ src/com/gerson/jike/tree/BinarySearchTree.java
@@ -31,6 +31,7 @@ public class BinarySearchTree<T extends Comparable> implements Tree<T> {
 
     /**
      * 前根遍历
+     *
      * @return
      */
     @Override
@@ -39,7 +40,8 @@ public class BinarySearchTree<T extends Comparable> implements Tree<T> {
     }
 
     /**
-     * 中跟遍历
+     * 中根遍历
+     *
      * @return
      */
     @Override
@@ -48,7 +50,8 @@ public class BinarySearchTree<T extends Comparable> implements Tree<T> {
     }
 
     /**
-     * 后跟遍历
+     * 后根遍历
+     *
      * @return
      */
     @Override
@@ -66,15 +69,19 @@ public class BinarySearchTree<T extends Comparable> implements Tree<T> {
         if (data == null) {
             throw new RuntimeException("data can not be null!");
         }
-        //给root赋值，第一次插入时需要赋值
-        root = insert(data,root);
+        if (!(findNode(data) == null)) {
+            return;
+        }
         size++;
+        //给root赋值，第一次插入时需要赋值
+        root = insert(data, root);
     }
 
     /**
      * 插入操作，递归实现
-     * @param data  插入节点的data值
-     * @param p 根节点、递归出口返回叶子节点
+     *
+     * @param data 插入节点的data值
+     * @param p    根节点、递归出口返回叶子节点
      * @return
      */
     private BinaryTreeNode<T> insert(T data, BinaryTreeNode p) {
@@ -86,10 +93,10 @@ public class BinarySearchTree<T extends Comparable> implements Tree<T> {
         int compareResult = data.compareTo(p.getVal());
         if (compareResult > 0) {
             //左子树
-            p.setLeft(insert(data,p.getLeft()));
+            p.setRight(insert(data, p.getRight()));
         } else if (compareResult < 0) {
             //右子树
-            p.setRight(insert(data,p.getLeft()));
+            p.setLeft(insert(data, p.getLeft()));
         }
         return p;
     }
@@ -97,40 +104,146 @@ public class BinarySearchTree<T extends Comparable> implements Tree<T> {
     /**
      * 递归删除
      * 使用被删除元素的右子树中最小的元素替换被删除元素，然后递归处理
+     *
      * @param data
      * @return
      */
     @Override
     public boolean remove(T data) {
-
+        BinaryTreeNode<T> parent = null;
+        BinaryTreeNode<T> targetNode = this.root;
+        if (targetNode == null) {
+            return false;
+        }
+        //寻找data对应的节点和其parent节点
+        boolean isRightChild = false;
+        while (targetNode != null) {
+            if (targetNode.getLeft() != null && data.compareTo(targetNode.getVal()) < 0) {
+                //优先处理data小于左子树根节点的情况
+                parent = targetNode;
+                targetNode = targetNode.getLeft();
+                isRightChild = false;
+            } else if (targetNode.getRight() != null && data.compareTo(targetNode.getVal()) > 0) {
+                parent = targetNode;
+                targetNode = targetNode.getRight();
+                isRightChild = true;
+            } else {
+                break;
+            }
+        }
+        if (targetNode.getVal() != data) {
+            //未找到节点
+            return false;
+        }
+        if (targetNode == this.root) {
+            //目标节点是根节点
+            BinaryTreeNode<T> successor = generateSuccessor(targetNode);
+            if (successor != null) {
+                findMin(successor).setLeft(this.root.getLeft());
+            }
+            this.root = successor;
+        } else if (targetNode.isLeaf()) {
+            //叶子节点
+            if (isRightChild) {
+                parent.setRight(null);
+            } else {
+                parent.setLeft(null);
+            }
+        } else {
+            //从右子树中寻找后继子树
+            BinaryTreeNode<T> successor = generateSuccessor(targetNode);
+            if (isRightChild) {
+                parent.setRight(successor);
+            } else {
+                parent.setLeft(successor);
+            }
+        }
+        size--;
+        return true;
     }
 
     @Override
     public T findMin() {
-        if (this.root == null) {
+        BinaryTreeNode<T> curNode = this.findMin(this.root);
+        return curNode != null ? curNode.getVal() : null;
+    }
+
+    /**
+     * 重构node子树,找到node右子树中最小的节点作为根构造一棵树
+     * @param node
+     * @return
+     */
+    private BinaryTreeNode<T> generateSuccessor(BinaryTreeNode<T> node) {
+        if (node == null || node.getRight() == null) {
             return null;
         }
-        BinaryTreeNode<T> curNode = root;
-        while(curNode.getLeft() != null) {
+//        if (node.getRight() == null) {
+//            return node.getLeft();
+//        }
+        //从右子树开始寻找
+        BinaryTreeNode<T> successor = node.getRight();
+        BinaryTreeNode<T> parent = null;
+        while (successor.getLeft() != null) {
+            parent = successor;
+            successor = successor.getLeft();
+        }
+        if (parent != null) {
+            parent.setLeft(successor.getRight());
+        }
+        if (successor != node.getRight()) {
+            successor.setRight(node.getRight());
+        }
+        successor.setLeft(node.getLeft());
+        return successor;
+    }
+
+    private BinaryTreeNode<T> findMin(BinaryTreeNode<T> node) {
+        if (node == null) {
+            return null;
+        }
+        BinaryTreeNode<T> curNode = node;
+        while (curNode.getLeft() != null) {
             curNode = curNode.getLeft();
         }
-        return curNode.getVal();
+        if (curNode == null) {
+            curNode = node.getRight();
+        }
+        return curNode;
     }
 
     @Override
     public T findMax() {
-        if (this.root == null) {
+        BinaryTreeNode<T> curNode = this.findMax(this.root);
+        return curNode != null ? curNode.getVal() : null;
+    }
+
+    public BinaryTreeNode findMax(BinaryTreeNode<T> root) {
+        if (root == null) {
             return null;
         }
         BinaryTreeNode<T> curNode = root;
-        while(curNode.getRight() != null) {
+        while (curNode.getRight() != null) {
             curNode = curNode.getRight();
         }
-        return curNode.getVal();
+        if (curNode == null) {
+            curNode = curNode.getLeft();
+        }
+        return curNode;
     }
 
     @Override
     public BinaryTreeNode<T> findNode(T data) {
+        BinaryTreeNode<T> curNode = this.root;
+        while(curNode != null) {
+            int compareResult = data.compareTo(curNode.getVal());
+            if (compareResult == 0) {
+                return curNode;
+            } else if (compareResult > 0){
+                curNode = curNode.getRight();
+            } else if (compareResult < 0){
+                curNode = curNode.getLeft();
+            }
+        }
         return null;
     }
 
--- src/com/gerson/jike/tree/BinarySearchTreeTest.java
+++ src/com/gerson/jike/tree/BinarySearchTreeTest.java
@@ -2,6 +2,8 @@ package com.gerson.jike.tree;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 /**
  * @author gezz
  * @description todo
@@ -12,14 +14,51 @@ public class BinarySearchTreeTest {
     public void testInsert() {
         BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
         binarySearchTree.insert(6);
-        binarySearchTree.insert(1);
-        binarySearchTree.insert(2);
+        binarySearchTree.insert(7);
         binarySearchTree.insert(3);
+        binarySearchTree.insert(2);
+        binarySearchTree.insert(8);
         binarySearchTree.insert(4);
         binarySearchTree.insert(5);
-        binarySearchTree.insert(7);
-        binarySearchTree.insert(8);
+        binarySearchTree.insert(1);
         binarySearchTree.insert(9);
-        binarySearchTree.insert(9);
+        binarySearchTree.remove(3);
+
+        binarySearchTree.insert(3);
+        for (int i = 1; i <= 9; i++) {
+            BinaryTreeNode<Integer> binaryTreeNode = binarySearchTree.findNode(i);
+            if (binaryTreeNode != null) {
+                System.out.println(binaryTreeNode.getVal());
+            }
+        }
+        System.out.println("max:" + binarySearchTree.findMax());
+        System.out.println("min:" + binarySearchTree.findMin());
+
+        while (binarySearchTree.size() > 0) {
+            Integer max = binarySearchTree.findMax();
+            binarySearchTree.remove(max);
+        }
+
+    }
+
+    @Test
+    public void insertRandom() {
+        BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
+        while (binarySearchTree.size() < 100) {
+            int random = new Random().nextInt(100);
+            System.out.println("insert:" + random);
+            binarySearchTree.insert(random);
+        }
+
+        while (binarySearchTree.size() > 10) {
+            Integer max = binarySearchTree.findMax();
+            if (max != null) {
+                System.out.println("remove:" + max);
+            }
+            binarySearchTree.remove(max);
+        }
+
+        System.out.println("max:" + binarySearchTree.findMax());
+        System.out.println("min:" + binarySearchTree.findMin());
     }
 }
