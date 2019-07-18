0cb18efc27aaa62a1a1efc82f50159fab087ec14
gezhizheng <gezhizheng@mvp01.com>
2019-07-18 15:51:48 +0800

    递归：求n的阶乘

---
+++ src/com/gerson/jike/Factorial.java
@@ -0,0 +1,22 @@
+package com.gerson.jike;
+
+import org.junit.Test;
+
+/**
+ * @author gezz
+ * @description todo
+ * @date 2019/7/18.
+ */
+public class Factorial {
+    public int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorial(n -1);
+    }
+
+    @Test
+    public void test() {
+        System.out.println(factorial(4));
+    }
+}
