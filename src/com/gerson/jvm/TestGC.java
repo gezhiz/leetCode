6b579f3c701bccd098d7d590ca84d3b66f5469d3
gezhizheng <gezhizheng@58.com>
2020-04-03 23:51:14 +0800

    测试GC

--- src/com/gerson/jvm/TestGC.java
+++ src/com/gerson/jvm/TestGC.java
@@ -10,7 +10,7 @@ public class TestGC {
     private static int _1MB = 1 * 1024 * 1024;
 
     /**
-     * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:PrintGcDetails -XX:SurvivorRation=8
+     * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:+UseParNewGC
      * 堆总大小20M，新生代10M，老年代10M，Eden区:Survivor = 8 : 1，Eden区8MB, From Survivor 1MB, To Survivor 1MB
      * @param args
      */
@@ -20,11 +20,28 @@ public class TestGC {
         b2 = new byte[2 * _1MB];
         b3 = new byte[2 * _1MB];
         //出现一次Minor GC
+        //分配担保机制提前转移到老年代去，大对象直接进入了老年代
         b4 = new byte[4 * _1MB];
-        b4 = new byte[4 * _1MB];//大对象直接进入了老年代
-        b4 = new byte[4 * _1MB];
-        Thread.sleep(1000);
+//        b4 = new byte[4 * _1MB];
+        //老年代内存不足以分配，进行Full GC
+//        b4 = new byte[4 * _1MB];
     }
+
+    /**
+     *
+     -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:+UseParNewGC
+
+     [GC (Allocation Failure) [ParNew: 7495K->440K(9216K), 0.0096143 secs] 7495K->6584K(19456K), 0.0096501 secs] [Times: user=0.02 sys=0.00, real=0.00 secs]
+     Heap
+     par new generation   total 9216K, used 4701K [0x00000007bec00000, 0x00000007bf600000, 0x00000007bf600000)
+     eden space 8192K,  52% used [0x00000007bec00000, 0x00000007bf029140, 0x00000007bf400000)
+     from space 1024K,  43% used [0x00000007bf500000, 0x00000007bf56e2c0, 0x00000007bf600000)
+     to   space 1024K,   0% used [0x00000007bf400000, 0x00000007bf400000, 0x00000007bf500000)
+     tenured generation   total 10240K, used 6144K [0x00000007bf600000, 0x00000007c0000000, 0x00000007c0000000)
+     the space 10240K,  60% used [0x00000007bf600000, 0x00000007bfc00030, 0x00000007bfc00200, 0x00000007c0000000)
+     Metaspace       used 3134K, capacity 4494K, committed 4864K, reserved 1056768K
+     class space    used 347K, capacity 386K, committed 512K, reserved 1048576K
+     */
 
     /**
 
